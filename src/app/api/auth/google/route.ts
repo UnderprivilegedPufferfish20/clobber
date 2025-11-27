3f34171bdf65f7e034eb6a7e79b9444822c81117
UnderprivilegedPufferfish20 <devingarrity5@gmail.com>
2025-11-26 20:56:44 -0600

    change callback/google to google/callback

--- src/app/api/auth/google/route.ts
+++ src/app/api/auth/google/route.ts
@@ -4,7 +4,7 @@ import { OAuth2Client } from 'google-auth-library';
 const client = new OAuth2Client(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  `${process.env.NEXT_PUBLIC_APP_URL}/api/auth/callback/google`
+  `${process.env.NEXT_PUBLIC_APP_URL}/api/auth/google/callback`
 );
 
 export async function GET(request: NextRequest) {
