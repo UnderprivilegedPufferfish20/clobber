6ac918e85fe447ad0a7e9358b06f7292296b4036
UnderprivilegedPufferfish20 <devingarrity5@gmail.com>
2025-11-26 21:08:29 -0600

    fix other oauth route

--- src/app/api/auth/google/callback/route.ts
+++ src/app/api/auth/google/callback/route.ts
@@ -5,7 +5,7 @@ import prisma from '@/lib/db';
 const client = new OAuth2Client(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  `${process.env.NEXT_PUBLIC_APP_URL}/api/auth/callback/google`
+  `${process.env.NEXT_PUBLIC_APP_URL}/api/auth/google/callback`
 );
 
 export async function GET(request: NextRequest) {
